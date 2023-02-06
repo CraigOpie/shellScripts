a8e8cdb072ac34dfd2af641099e7d2af4a54bd06
Craig Opie <craig@deathstardev.com>
2023-02-06 06:59:20 -1000

    added usbscan

---
+++ bash_scripts/usbscan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This file is used to get USB mount points in /dev/ without question
+# this is a timesaver for those who know
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && exit
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && exit
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
