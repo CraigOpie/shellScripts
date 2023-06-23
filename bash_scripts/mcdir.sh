2ac8efb705e4b7f2f845d22a769de7f3a5a9b11a
Craig Opie <craig@deathstardev.com>
2023-06-23 17:34:40 +0000

    Improve readability and security

--- bash_scripts/mcdir.sh
+++ bash_scripts/mcdir.sh
@@ -1,116 +1,74 @@
 #!/bin/bash
 # This file is used to create a directory and then immediately cd into it
 # using pre-existing libraries to ensure robust application
+
 function mcdir() {
-  ## display the help menu
-  if [ $# -eq 0 ] || [ $1 = "-h" ] || [ $1 = "--help" ]
+  if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
     then
-      echo "Usage: [option...] {directory}"
-      echo "   -m, --mode                  Sets the access mode for the new directory     "
-      echo "   -p, --path                  Create all directories in path                 "
-      echo "   -h, --help                  Shows this helpful information                 "
+      echo "Usage: mcdir [-m MODE] [-p] DIRECTORY"
+      echo "Create a directory and change into it."
       echo
+      echo "Options:"
+      echo "   -m, --mode MODE     Sets the access mode for the new directory. Default is 755."
+      echo "   -p, --path          Create all directories in path."
+      echo "   -h, --help          Shows this helpful information."
+      echo
+      echo "Examples:"
+      echo "   mcdir -m 777 new_dir"
+      echo "   mcdir -p new_dir/sub_dir"
       return 0
   fi
 
-  ## set default variables
-  MODE=false
-  MKPATH=false
-  STDMK=true
-  ACCESS=777
-  DIR="$1"
+  # set default variables
+  local accessMode=755
+  local makePath=false
+  local directory
 
-  ## print version
-  if [ $1 = "--version" ]
-    then
-      echo "mcdir version 1.0.1"
-      return 0
-  fi
-
-  ## load the arguments
-  while [ "$1" != "" ]
-    do
-      DIR="$1"
-      if [ $1 = "-m" ] || [ $1 = "--mode" ]
-        then
-          shift
-          MODE=true
-          STDMK=false
-          ACCESS=$1
-      fi
-      if [ $1 = "-p" ] || [ $1 = "--path" ]
-        then
-          MKPATH=true
-          STDMK=false
-      fi
-      shift
+  while (( "$#" )); do
+    case "$1" in
+      -m|--mode)
+        if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+          accessMode=$2
+          shift 2
+        else
+          echo "Error: Argument for $1 is missing" >&2
+          exit 1
+        fi
+        ;;
+      -p|--path)
+        makePath=true
+        shift
+        ;;
+      -*|--*=) 
+        echo "Error: Unsupported flag $1" >&2
+        exit 1
+        ;;
+      *)
+        directory=$1
+        shift
+        ;;
+    esac
   done
 
-  ## execute function based on arguments provided
-  if [ "$MODE" = true ]
-    then
-      if [ "$MKPATH" = true ]
-        then
-          mkdir -m $ACCESS -p $DIR &>/dev/null
-          if [ $? -gt 0 ]
-            then
-              echo "There was a problem creating your directory."
-              return 1
-          fi
-          cd "$DIR" &>/dev/null
-          if [ $? -gt 0 ]
-            then
-              echo "Unable to change into that directory."
-              return 1
-          fi
-          return 0
-      fi
-      mkdir -m $ACCESS $DIR &>/dev/null
-      if [ $? -gt 0 ]
-        then
-          echo "There was a problem creating your directory."
-          return 1
-      fi
-      cd "$DIR" &>/dev/null
-      if [ $? -gt 0 ]
-        then
-          echo "Unable to change into that directory."
-          return 1
-      fi
-      return 0
+  # execute function based on arguments provided
+  local mkdirCommand="mkdir -m $accessMode"
+  
+  if [ "$makePath" = true ]; then
+    mkdirCommand+=" -p"
   fi
-  if [ "$MKPATH" = true ]
-    then
-      mkdir -p $DIR &>/dev/null
-      if [ $? -gt 0 ]
-        then
-          echo "There was a problem creating your directory."
-          return 1
-      fi
-      cd "$DIR" &>/dev/null
-      if [ $? -gt 0 ]
-        then
-          echo "Unable to change into that directory."
-          return 1
-      fi
-      return 0
+  
+  $mkdirCommand "$directory"
+  if [ $? -ne 0 ]; then
+    echo "There was a problem creating your directory."
+    return 1
   fi
-  if [ "$STDMK" = true ]
-    then
-      mkdir $DIR &>/dev/null
-      if [ $? -gt 0 ]
-        then
-          echo "There was a problem creating your directory."
-          return 1
-      fi
-      cd "$DIR" &>/dev/null
-      if [ $? -gt 0 ]
-        then
-          echo "Unable to change into that directory."
-          return 1
-      fi
-      return 0
+
+  cd "$directory"
+  if [ $? -ne 0 ]; then
+    echo "Unable to change into that directory."
+    return 1
   fi
+
   return 0
 }
 export -f mcdir
